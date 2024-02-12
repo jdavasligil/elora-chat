f33ba96398fa4fb04955ec24c05ac4d51b4b15e1
Hayden Price <hwplivnlow@gmail.com>
2024-02-12 11:30:50 -0700

    Clean up .env import in main.go

--- src/backend/main.go
+++ src/backend/main.go
@@ -6,15 +6,9 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/hpwn/EloraChat/src/backend/routes" // Ensure this is the correct path to your routes package
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	// Load environment variables from .env file
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 
 	// Create a new router
 	r := mux.NewRouter()
@@ -29,7 +23,7 @@ func main() {
 
 	// Start the server
 	log.Println("Starting server on :8080")
-	err = http.ListenAndServe(":8080", r) // Pass the router r here
+	err := http.ListenAndServe(":8080", r) // Pass the router r here
 	if err != nil {
 		log.Fatal(err)
 	}
