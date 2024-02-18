ff2ad9db26375eeb979c21aca645eb783e5c207b
Hayden Price <hwplivnlow@gmail.com>
2024-02-18 15:27:13 -0700

    main.go urls, local .svgs

--- src/backend/main.go
+++ src/backend/main.go
@@ -21,15 +21,22 @@ func main() {
 
 	// Start fetching chat messages
 	chatURLs := []string{
-		//"https://www.twitch.tv/hp_az",
-		//"https://www.youtube.com/@hp_az/live",
-		// "https://www.youtube.com/watch?v=jfKfPfyJRdk",
-		//"https://www.twitch.tv/Johnstone",
-		"https://www.twitch.tv/Hypnoshark",
-		//"https://www.twitch.tv/QTCinderella",
+		// "https://www.twitch.tv/hp_az",
+		// "https://www.youtube.com/@hp_az/live",
+		// "https://youtube.com/live/7NA555IYE24?feature=share",
+		// "https://youtube.com/live/7455sVTXUPU?feature=share",
+		// "https://www.youtube.com/watch?v=jfKfPfyJRdk", // lofi girl live
+		// "https://www.youtube.com/watch?v=39VeO9p7Vn0", // dayo live test stream
+		// "https://www.youtube.com/live/6sjf7R0o-ss?si=WkdXIOu83_7Sglk2&t=7500", // ludwig live test stream
+		// "https://www.twitch.tv/Johnstone",
+		// "https://www.twitch.tv/Hypnoshark",
+		// "https://www.twitch.tv/QTCinderella",
 		// "https://www.twitch.tv/Quin69",
 		// "https://www.twitch.tv/jakenbakeLIVE",
 		// "https://www.twitch.tv/Knut",
+		"https://www.youtube.com/@dayoman/live",
+		// "https://www.twitch.tv/dayoman",
+		// "https://www.twitch.tv/forsen", // basically a hard code for constant chats
 	}
 	routes.StartChatFetch(chatURLs)
 
