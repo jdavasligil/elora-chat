aa56978ab2d418326243838eaaec4292796d62f4
jdavasligil <22987230+jdavasligil@users.noreply.github.com>
2025-04-19 19:11:00 -0700

    Create abstract cache interface with redis implementation

---
+++ src/backend/cache/cache.go
@@ -0,0 +1,37 @@
+package cache
+
+import (
+	"time"
+)
+
+type CacheStrategy byte
+
+const (
+	StratRedis CacheStrategy = iota
+	StratDB
+	StratLocal
+)
+
+type CacheMessage struct {
+	ID    string
+	Value interface{}
+}
+
+// Cache supports key/value storage and append-only logging and streaming
+type Cache interface {
+	// Keystore operations
+
+	Get(key string) (string, error)
+	Set(key string, value string, expiration time.Duration) error
+	Del(keys ...string) error
+
+	// Stream operations
+
+	XAdd(stream string, values map[string]interface{}, maxlen int64) (string, error)
+	XGetNew(stream string, key string) ([]CacheMessage, error)
+	XGetLastN(stream string, key string, count int64) ([]CacheMessage, error)
+
+	// Connection
+
+	Ping() (string, error)
+}
---
+++ src/backend/cache/rediscache.go
@@ -0,0 +1,104 @@
+package cache
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+// RedisCache is a thin layer over redis that implements the Cache interface.
+type RedisCache struct {
+	ctx    context.Context
+	client redis.Client
+	lastID []byte
+}
+
+func NewRedisCache(ctx context.Context, opt *redis.Options) *RedisCache {
+	return &RedisCache{
+		ctx:    ctx,
+		client: *redis.NewClient(opt),
+		lastID: make([]byte, 0, 20), // 20 digits in 64 bit max
+	}
+}
+
+func (c RedisCache) Get(key string) (string, error) {
+	return c.client.Get(c.ctx, key).Result()
+}
+
+func (c RedisCache) Set(key string, value string, expiration time.Duration) error {
+	return c.client.Set(c.ctx, key, value, expiration).Err()
+}
+
+func (c RedisCache) Del(keys ...string) error {
+	return c.client.Del(c.ctx, keys...).Err()
+}
+
+func (c RedisCache) XAdd(stream string, values map[string]interface{}, maxlen int64) (string, error) {
+	return c.client.XAdd(c.ctx, &redis.XAddArgs{
+		Stream: stream,
+		Values: values,
+		MaxLen: maxlen,
+		Approx: true,
+	}).Result()
+}
+
+// Get the newest value from a stream.
+func (c RedisCache) XGetNew(stream string, key string) ([]CacheMessage, error) {
+	streams, err := c.client.XRead(c.ctx, &redis.XReadArgs{
+		Streams: []string{stream, string(c.lastID)},
+		Block:   0,
+	}).Result()
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(streams) == 0 {
+		return nil, errors.New(fmt.Sprintf("Stream: [%s] not found", stream))
+	}
+
+	s := streams[0]
+	values := make([]CacheMessage, len(s.Messages))
+	for i, message := range s.Messages {
+		values[i].ID = message.ID
+		values[i].Value = message.Values[key]
+	}
+
+	copy(c.lastID, values[len(values)-1].ID)
+
+	return values, nil
+}
+
+// Get the last N values from a stream sorted by newest.
+func (c RedisCache) XGetLastN(stream string, key string, count int64) ([]CacheMessage, error) {
+	messages, err := c.client.XRevRangeN(c.ctx, stream, "+", "-", count).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	values := make([]CacheMessage, len(messages))
+
+	vIdx := 0
+	lastID := "0"
+	for i := len(messages) - 1; i >= 0; i-- {
+		message := messages[i]
+
+		values[vIdx].ID = message.ID
+		values[vIdx].Value = message.Values[key]
+
+		if message.ID > lastID {
+			lastID = message.ID
+		}
+	}
+
+	copy(c.lastID, lastID)
+
+	return values, nil
+}
+
+func (c RedisCache) Ping() (string, error) {
+	return c.client.Ping(c.ctx).Result()
+}
