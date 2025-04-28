392d34a58d342340eeba2ee03137fffa48e175bb
jdavasligil <22987230+jdavasligil@users.noreply.github.com>
2025-04-27 21:47:10 -0700

    Recursive effect tokenization

--- src/backend/routes/token.go
+++ src/backend/routes/token.go
@@ -13,6 +13,8 @@ const (
 	TokenTypeEffect = "effect"
 )
 
+const TextEffectSep = ":"
+
 var TextEffects = map[string]struct{}{
 	"wave":   {},
 	"wave2":  {},
@@ -22,20 +24,20 @@ var TextEffects = map[string]struct{}{
 }
 
 var TextColours = map[string]struct{}{
-	"yellow": {},
-	"red":    {},
-	"green":  {},
-	"cyan":   {},
-	"purple": {},
-	"white":  {},
-	"flash1": {},
-	"flash2": {},
-	"flash3": {},
-	"glow1":  {},
-	"glow2":  {},
-	"glow3":  {},
-	"rainbow":  {},
-	"pattern":  {},
+	"yellow":  {},
+	"red":     {},
+	"green":   {},
+	"cyan":    {},
+	"purple":  {},
+	"white":   {},
+	"flash1":  {},
+	"flash2":  {},
+	"flash3":  {},
+	"glow1":   {},
+	"glow2":   {},
+	"glow3":   {},
+	"rainbow": {},
+	"pattern": {},
 }
 
 type Token struct {
@@ -45,7 +47,61 @@ type Token struct {
 }
 
 type Tokenizer struct {
-	EmoteCache map[string]Emote
+	EmoteCache map[string]*Emote
+}
+
+// Recursively check for the presence of colors and effects
+//
+// Formats:
+//
+//	color:text
+//	effect:text
+//	color:effect:text
+//	effect:colour:text
+func (p Tokenizer) iterWordEffect(yield func(Token) bool, word string, depth int) {
+
+	// Base Case: Empty string
+	if word == "" {
+		return
+	}
+
+	tok := Token{Type: TokenTypeText, Text: word}
+
+	// Base Case: Emote
+	if emote, ok := p.EmoteCache[word]; ok {
+		tok.Type = TokenTypeEmote
+		tok.Emote = emote
+		yield(tok)
+		return
+	}
+
+	// Base Case: Depth limit
+	if depth == 2 {
+		yield(tok)
+		return
+	}
+
+	prefix, postfix, sepFound := strings.Cut(word, TextEffectSep)
+
+	if !sepFound || prefix == "" {
+		yield(tok)
+		return
+	}
+
+	if _, ok := TextColours[prefix]; ok {
+		tok.Type = TokenTypeColour
+	} else if _, ok := TextEffects[prefix]; ok {
+		tok.Type = TokenTypeEffect
+	} else {
+		yield(tok)
+		return
+	}
+
+	tok.Text = prefix
+	yield(tok)
+
+	// Recursively tokenize next effect
+	p.iterWordEffect(yield, postfix, depth+1)
 }
 
 // Returns an iterator over the string which yields tokens.
@@ -53,23 +109,30 @@ func (p Tokenizer) Iter(s string) iter.Seq[Token] {
 	return func(yield func(Token) bool) {
 		scanner := bufio.NewScanner(strings.NewReader(s))
 		scanner.Split(bufio.ScanWords)
+
+		// Check first word for color or effects
+		ok := scanner.Scan()
+		if !ok {
+			return
+		}
+		word := scanner.Text()
+
+		// Recursively tokenize text effects
+		p.iterWordEffect(yield, word, 0)
+
+		// Scan the rest of the message for emotes
 		for scanner.Scan() {
 			word := scanner.Text()
+
+			// Assume word is text
 			tok := Token{Type: TokenTypeText, Text: word}
 
+			// Check for emote
 			if emote, ok := p.EmoteCache[word]; ok {
 				tok.Type = TokenTypeEmote
-				tok.Emote = &emote
-			} else if word[len(word)-1] == ':' {
-				text := word[:len(word)-1]
-				if _, ok := TextEffects[text]; ok {
-					tok.Type = TokenTypeEffect
-					tok.Text = text
-				} else if _, ok := TextColours[text]; ok {
-					tok.Type = TokenTypeColour
-					tok.Text = text
-				}
+				tok.Emote = emote
 			}
+
 			if !yield(tok) {
 				return
 			}
---
+++ src/backend/routes/token_test.go
@@ -0,0 +1,103 @@
+package routes
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTokenizer(t *testing.T) {
+	tokenizer := Tokenizer{
+		EmoteCache: map[string]*Emote{
+			"Clap": {
+				ID:     "1",
+				Name:   "Clap",
+				Images: []Image{{URL: "https://cdn.test.net/v1/emotes/1/1x.webp"}},
+			},
+			"Clap2": {
+				ID:     "2",
+				Name:   "Clap2",
+				Images: []Image{{URL: "https://cdn.test.net/v1/emotes/2/1x.webp"}},
+			},
+			"KEKW": {
+				ID:     "3",
+				Name:   "KEKW",
+				Images: []Image{{URL: "https://cdn.test.net/v1/emotes/3/1x.webp"}},
+			},
+			"FeelsGoodMan": {
+				ID:     "4",
+				Name:   "FeelsGoodMan",
+				Images: []Image{{URL: "https://cdn.test.net/v1/emotes/4/1x.webp"}},
+			},
+		},
+	}
+	tests := []struct {
+		Message  string
+		Expected []Token
+	}{
+		{// Gibberish with whitespace
+			Message: "  2[qrp]3-4t[ #(YT$ jd  ",
+			Expected: []Token{
+				{TokenTypeText, "2[qrp]3-4t[", nil},
+				{TokenTypeText, "#(YT$", nil},
+				{TokenTypeText, "jd", nil},
+			},
+		},
+		{// Emotes
+			Message: "KEKW KEKW FeelsGoodMan !!!",
+			Expected: []Token{
+				{TokenTypeEmote, "KEKW", tokenizer.EmoteCache["KEKW"]},
+				{TokenTypeEmote, "KEKW", tokenizer.EmoteCache["KEKW"]},
+				{TokenTypeEmote, "FeelsGoodMan", tokenizer.EmoteCache["FeelsGoodMan"]},
+				{TokenTypeText, "!!!", nil},
+			},
+		},
+		{ // Semicolons all over
+			Message: ":jaja::#:!@#:",
+			Expected: []Token{
+				{TokenTypeText, ":jaja::#:!@#:", nil},
+			},
+		},
+		{ // colour:text
+			Message: "white:text",
+			Expected: []Token{
+				{TokenTypeColour, "white", nil},
+				{TokenTypeText, "text", nil},
+			},
+		},
+		{ // colour:<WS>text
+			Message: "white: text",
+			Expected: []Token{
+				{TokenTypeColour, "white", nil},
+				{TokenTypeText, "text", nil},
+			},
+		},
+		{ // colour:effect:emote
+			Message: "rainbow:wave2:KEKW",
+			Expected: []Token{
+				{TokenTypeColour, "rainbow", nil},
+				{TokenTypeEffect, "wave2", nil},
+				{TokenTypeEmote, "KEKW", tokenizer.EmoteCache["KEKW"]},
+			},
+		},
+		{ // effect:colour:emote
+			Message: "wave2:rainbow:KEKW",
+			Expected: []Token{
+				{TokenTypeEffect, "wave2", nil},
+				{TokenTypeColour, "rainbow", nil},
+				{TokenTypeEmote, "KEKW", tokenizer.EmoteCache["KEKW"]},
+			},
+		},
+	}
+	for count, test := range tests {
+		iterator := tokenizer.Iter(test.Message)
+		tokens := []Token{}
+		for tok := range iterator {
+			tokens = append(tokens, tok)
+		}
+		if !reflect.DeepEqual(tokens, test.Expected) {
+			t.Logf("Test [%d] FAILED:\n", count)
+			t.Logf("\nExpected: %v\n\n Got: %v\n", test.Expected, tokens)
+			t.Fail()
+		}
+	}
+}
