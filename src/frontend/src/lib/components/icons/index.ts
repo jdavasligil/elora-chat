f511ce1dbb5dffb5f4ff066649284239b2cf09c6
Connor Younglund <cdy@airmail.cc>
2025-05-03 16:29:30 -0700

    replace the old frontend

--- src/frontend/src/lib/components/icons/index.ts
+++ src/frontend/src/lib/components/icons/index.ts
@@ -1,2 +1,2 @@
 export { default as TwitchIcon } from './TwitchIcon.svelte';
-export { default as YoutubeIcon } from './YoutubeIcon.svelte';
+export { default as YoutubeIcon } from './YouTubeIcon.svelte';
--- src/frontend/src/lib/config.ts
+++ src/frontend/src/lib/config.ts
@@ -1,2 +1,2 @@
 export const deployedUrl = 'https://elorachat.wizg.xyz';
-export const useDeployedApi = true; // Be sure to set this to true when building/deploying
+export const useDeployedApi = false; // Be sure to set this to true when building/deploying
