59186db2de31c84efbbf462303b08cd207d7bb76
Connor Younglund <cdy@airmail.cc>
2025-05-03 16:40:26 -0700

    implement message sending

--- src/frontend/src/lib/components/SendMessage.svelte
+++ src/frontend/src/lib/components/SendMessage.svelte
@@ -1,7 +1,46 @@
-<div id="message-send-container">
-  <input id="message-input" type="text" placeholder="Type a message..." />
-  <button id="send-message-button">send</button>
-</div>
+<script lang="ts">
+  import { authState } from '$lib/api/auth.svelte';
+  import { buildApiUrl } from '$lib/utils';
+
+  let message = '';
+
+  function sendMessage(event: Event) {
+    event.preventDefault();
+    if (!authState.loggedIn) return;
+
+    // Check if the message is empty
+    if (!message) {
+      console.log('No message to send');
+      return;
+    }
+
+    // Send the message
+    fetch(buildApiUrl('/auth/send-message'), {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ message }),
+      credentials: 'include' // Important for session handling
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log('Message sent successfully');
+        } else {
+          console.error('Failed to send message');
+        }
+      })
+      .catch((error) => console.error('Error sending message:', error));
+
+    // Clear message input
+    message = '';
+  }
+</script>
+
+<form id="message-send-container" on:submit={sendMessage}>
+  <input id="message-input" type="text" placeholder="Type a message..." bind:value={message} />
+  <button id="send-message-button" type="submit" disabled={!authState.loggedIn}>send</button>
+</form>
 
 <style lang="scss">
   #message-send-container {
