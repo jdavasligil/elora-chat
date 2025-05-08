5cae9687dbd24e43551ace5384b9646738b06ec3
jdavasligil <22987230+jdavasligil@users.noreply.github.com>
2025-05-08 15:18:09 -0700

    Add chat pause on hover

---
+++ src/frontend/src/lib/components/PauseOverlay.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  let { newMessageCount }: { newMessageCount: number } = $props();
+</script>
+
+<div id="pauseOverlay">
+  {#if newMessageCount === 0}
+    <p>Chat Paused</p>
+  {:else if newMessageCount === 1}
+    <p>1 New Message</p>
+  {:else}
+    <p>{newMessageCount} New Messages</p>
+  {/if}
+</div>
+
+<style lang="scss">
+  #pauseOverlay {
+    position: absolute;
+    top: 80%;
+    left: 50%;
+    transform: translate(-50%, -80%);
+    z-index: 1000;
+    text-align: center;
+    border: 1px solid #242729;
+    border-radius: 8px;
+    background-color: rgba(12, 15, 18, 0.8);
+    font-weight: bold;
+    padding: 8px;
+  }
+</style>
